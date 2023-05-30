4d9d46ffd8266e1dfa009d3efdcd33ab518ed2df
Iconframework <it@iconframework.com>
2023-05-30 11:33:19 +0700

    Edit git action version .net

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace WebApplication1.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            Console.WriteLine("Test");
+            Console.WriteLine("Test1");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
